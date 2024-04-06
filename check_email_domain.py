07f8704363f5642909f275fa604129980073a72e
_saurabhh_sawant <ssawant@dnyanyog.org>
2024-04-06 15:32:46 +0530

    test: Add test commit
    Should not pushed

---
+++ check_email_domain.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import subprocess
+
+expectedEmail = "ssawant@dnyanyog.org"
+
+email = subprocess.run(["git", "config", "user.email"], capture_output=True, text=True).stdout.strip()
+
+if email == expectedEmail:
+    print("You are using email as per configuration, proceeding to commit")
+    exit(0)
+else:
+    print(f"Configured email is not as per config. It should be {expectedEmail}")
+    exit(1)
+
